46f6f25328c3982585839a9714c3fc4ce04c7a60
delianapujiarti <delianapujiarti38@gmail.com>
2017-05-02 11:30:32 +0700

    belajar requile

---
+++ belajar/tabel.php
@@ -0,0 +1,17 @@
+<table border="1">
+	<thead >
+		<tr>
+			<td>No</td>
+			<td>NIM</td>
+			<td>Nama</td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td>H1101151018</td>
+			<td>Deliana pujiarti</td>
+		</tr>
+	</tbody>
+
+</table>
